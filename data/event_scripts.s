eab46d46efb3c65e129f291eaafae1a7a77f9480
Nathan Gil <darthmavoc@gmail.com>
2023-11-15 16:49:04 +1100

    Starting map setup

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,3 +1059,13 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/A3_Moorol_Yuang/scripts.inc"
+
+	.include "data/maps/A4_Moorol_Yuang/scripts.inc"
+
+	.include "data/maps/A5_Moorol_Yuang/scripts.inc"
+
+	.include "data/maps/A6_Ferrogum_Desert/scripts.inc"
+
+	.include "data/maps/A7_Ferrogum_Desert/scripts.inc"
